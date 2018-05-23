b62b4ed23efd75937abf6a2b2e89d2941762ace6
Dainius <pastas4@gmail.com>
2018-05-23 15:24:29 +0200

    Allow each worker to access the optimised packages

--- src/detect-breaks.r
+++ src/detect-breaks.r
@@ -168,6 +168,7 @@ SparkCalc = function(input_raster, fx, filename, mem_usage=0.9*1024^3, datatype=
         
         # Process the block
         print(paste0("Chunk ", Index, "/", NumChunks, ": processing to ", ResultFilenames[Index]))
+        .libPaths(c("/data/users/Public/greatemerald/r-packages", .libPaths()))
         library(strucchange)
         library(bfast)
         if (!is.null(datatype))
