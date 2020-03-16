e2a544476315f283589ce1719f28f8acc9a95a55
GreatEmerald <pastas4@gmail.com>
2020-03-16 11:20:53 +0100

    Switch to pbapply to speed up centroid calculation

--- src/centroids/add-centroids.r
+++ src/centroids/add-centroids.r
@@ -1,9 +1,11 @@
+library(pbapply)
+
 # Add 300 m centroids to reference data
 AddCentroids = function(df, grid=300)
 {
-    CentroidStrings = apply(df, 1, function(row){
+    CentroidStrings = pbapply(df, 1, function(row){
         system(paste0("python centroids/identify_centroids_LC100_grid.py ", row[["x"]], " ", row[["y"]], " | grep -Po '(?<=", grid, "m PROBA-V UTM pixel: ).*'"), TRUE)
-        })
+        }, cl=4)
     CentroidMatrix = apply(do.call(rbind, strsplit(c(CentroidString, CentroidString), ", ")), 2, as.numeric)
     return(cbind(df, centroid_x=CentroidMatrix[,1], centroid_y=CentroidMatrix[,2]))
 }
